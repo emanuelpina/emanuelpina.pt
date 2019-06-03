9bd0dc3da1f73f2051f221f8d1fb89bb78b01fa7
Emanuel Pina <20098990+emanuelpina@users.noreply.github.com>
2019-06-03 12:18:12 +0100

    Update Staticman with more fields

--- themes/mytheme/assets/js/custom.js
+++ themes/mytheme/assets/js/custom.js
@@ -83,10 +83,13 @@ $(document).on('click', '.to_top', function(e) {
 
 // Staticman
 $(document).on('click', '.post-comment-reply-button', function() {
-    var replyTo = $(this).attr('replyto'),
-        replyName = $(this).attr('replyto-name');
-    $('.post-comments-form input[name="fields[reply_to]"]').val(replyTo);
-    $('.post-comments-form-title').html('Responder ' + replyName);
+    var replyId = $(this).attr('replyto'),
+        replyThread = $(this).attr('replythread');
+        replyName = $(this).attr('replyname');
+    $('.post-comments-form input[name="fields[replyId]"]').val(replyId);
+    $('.post-comments-form input[name="fields[replyThread]"]').val(replyThread);
+    $('.post-comments-form input[name="fields[replyName]"]').val(replyName);
+    $('.post-comments-form-title').html('Responder a ' + replyName);
     $('html, body').animate({
         scrollTop: $('.post-comments-form-title').offset().top - 85
     }, 200);
@@ -94,11 +97,13 @@ $(document).on('click', '.post-comment-reply-button', function() {
  });
 
  $(document).on('click', '.post-comments-form-cancel', function() {
-    var commentsThread = $('.post-comments-form input[name="fields[reply_to]"]').val();
-    $('.post-comments-form input[name="fields[reply_to]"]').val('');
+    var replyThread = $('.post-comments-form input[name="fields[replyThread]"]').val();
+    $('.post-comments-form input[name="fields[replyId]"]').val('');
+    $('.post-comments-form input[name="fields[replyThread]"]').val('');
+    $('.post-comments-form input[name="fields[replyName]"]').val('');
     $('.post-comments-form-title').html('Deixe um comentário');
     $('html, body').animate({
-        scrollTop: $('#' + commentsThread).offset().top - 85
+        scrollTop: $('#' + replyThread).offset().top - 85
     }, 200);
     $('.post-comments-form-cancel').hide();
  });
