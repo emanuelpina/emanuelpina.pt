34b79106be7643ba161fb0c02e48f48c6ca66365
Emanuel Pina <20098990+emanuelpina@users.noreply.github.com>
2019-09-21 16:44:46 +0100

    Add link tags to headings on post-content

--- themes/mytheme/assets/js/custom.js
+++ themes/mytheme/assets/js/custom.js
@@ -24,7 +24,6 @@ $(document).on('click', '.menu-triggered', function() {
 $(document).on('click', '.has-children > a', function(e) {
     var isTouch = ('ontouchstart' in document.documentElement);
     if ( isTouch ){
-        console.log('Is touch');
         e.preventDefault();
     }
 });
@@ -87,3 +86,12 @@ $(document).on('click', '.to_top', function(e) {
     e.preventDefault();
     $("html, body").animate({scrollTop: 0}, scrollspeed);
  });
+
+$('.post-content > h3').each(function(){
+    var headingid = $(this).attr('id');
+    var isTouch = ('ontouchstart' in document.documentElement);
+    $(this).append('<a class="post-heading-link" href="#' + headingid + '"><svg class="icon-link" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M6.879 9.934c-0.208 0-0.416-0.079-0.575-0.238-1.486-1.486-1.486-3.905 0-5.392l3-3c0.72-0.72 1.678-1.117 2.696-1.117s1.976 0.397 2.696 1.117c1.486 1.487 1.486 3.905 0 5.392l-1.371 1.371c-0.317 0.317-0.832 0.317-1.149 0s-0.317-0.832 0-1.149l1.371-1.371c0.853-0.853 0.853-2.241 0-3.094-0.413-0.413-0.963-0.641-1.547-0.641s-1.134 0.228-1.547 0.641l-3 3c-0.853 0.853-0.853 2.241 0 3.094 0.317 0.317 0.317 0.832 0 1.149-0.159 0.159-0.367 0.238-0.575 0.238z"></path><path d="M4 15.813c-1.018 0-1.976-0.397-2.696-1.117-1.486-1.486-1.486-3.905 0-5.392l1.371-1.371c0.317-0.317 0.832-0.317 1.149 0s0.317 0.832 0 1.149l-1.371 1.371c-0.853 0.853-0.853 2.241 0 3.094 0.413 0.413 0.962 0.641 1.547 0.641s1.134-0.228 1.547-0.641l3-3c0.853-0.853 0.853-2.241 0-3.094-0.317-0.317-0.317-0.832 0-1.149s0.832-0.317 1.149 0c1.486 1.486 1.486 3.905 0 5.392l-3 3c-0.72 0.72-1.678 1.117-2.696 1.117z"></path></svg></a>');
+    if ( isTouch ){
+        $('.post-heading-link').css("display", "inline");
+    }
+});
