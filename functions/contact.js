c23cf9e0dfb55148e5b690fd8bfd1087b3cd949d
Emanuel Pina <20098990+emanuelpina@users.noreply.github.com>
2019-09-24 00:33:13 +0100

    Change both email messages

--- functions/contact.js
+++ functions/contact.js
@@ -5,14 +5,19 @@ exports.handler = function(event, context, callback) {
     const name = body.data.Name.trim();
     const email = body.data.Email.trim();
     const subject = body.data.Subject.trim();
-    const message = body.data.Message.trim();
     const mailto = process.env.CONTACT_MAIL_TO;
     const mailfrom = process.env.CONTACT_MAIL_FROM;
+    const dataArray = Object.entries(body.human_fields);
     console.log('Variables are loaded...');
-    const htmlone = `${name},<br><br>You are receiving this email because you sent me a message through a form on my blog.<br><br><u>If you want to add further details, simply reply to this e-mail.</u><br><br>Below is a copy of your message:<br><blockquote>--------------------<br><b>Name:</b> ${name}<br><b>Email:</b> ${email}<br><b>Subject:</b> ${subject}<br><b>Message</b><br>${message}<br>--------------------</blockquote><br>I will reply as soon as possible.<br><br>Sincerly,<br>Emanuel Pina<br>emanuelpina.pt`;
-    const textone = ` ${name},\r\n\r\n You are receiving this email because you sent me a message through a form on my blog.\r\n\r\n If you want to add further details, simply reply to this e-mail.\r\n\r\n Below is a copy of your message:\r\n --------------------\r\n Name: ${name}\r\n Email: ${email}\r\n Subject: ${subject}\r\n Message\r\n ${message}\r\n --------------------\r\n\r\n I will reply as soon as possible.\r\n\r\n Sincerly,\r\n Emanuel Pina\r\n emanuelpina.pt`;
-    const htmltwo = `You have received a new message.<br><blockquote>--------------------<br><b>Name:</b> ${name}<br><b>Email:</b> ${email}<br><b>Subject:</b> ${subject}<br><b>Message</b><br>${message}<br>--------------------</blockquote><br>Best regards,<br>The Post Office`;
-    const texttwo = ` You have received a new message.\r\n\r\n --------------------\r\n Name: ${name}\r\n Email: ${email}\r\n Subject: ${subject}\r\n Message\r\n ${message}\r\n --------------------\r\n\r\n Best regards,\r\n The Post Office`;
+    const tableData = dataArray
+        .map(x => `<tr><td><b>${x[0]}</b>&nbsp;</td><td>${x[1]}</td></td>`)
+        .join('');
+    const textData = dataArray.map(x => ` ${x[0]}: ${x[1]}\r\n`).join('');
+    const htmlone = `${name},<br><br>You are receiving this email because you sent me a message through a form on my blog.<br><br><u>If you want to add further details, simply reply to this e-mail.</u><br><br>A copy of your message:<br><table><tbody>${tableData}</tbody></table><br>I will reply as soon as possible.<br><br>Sincerly,<br>Emanuel Pina<br>emanuelpina.pt`;
+    const htmltwo = `You have received a new message.<br><br><table><tbody>${tableData}</tbody></table><br>Best regards,<br>The Post Office`;
+    const textone = ` ${name},\r\n\r\n You are receiving this email because you sent me a message through a form on my blog.\r\n\r\n If you want to add further details, simply reply to this e-mail.\r\n\r\n A copy of your message:\r\n${textData}\r\n I will reply as soon as possible.\r\n\r\n Sincerly,\r\n Emanuel Pina\r\n emanuelpina.pt`;
+    const texttwo = ` You have received a new message.\r\n\r\n${textData}\r\n Best regards,\r\n The Post Office`;
+    console.log('Messages are ready...');
     const msgs = [
         {
         to: email,
@@ -47,7 +52,7 @@ exports.handler = function(event, context, callback) {
         statusCode: 200,
         body: JSON.stringify(msgs),
       });
-    console.log('Messages are sent!');
+    console.log('Emails are sent!');
     })
     .catch(error => callback(error));
 };
