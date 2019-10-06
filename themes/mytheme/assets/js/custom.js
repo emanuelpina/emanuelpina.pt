a28c40c6802407e592ab3af373329f3781c53cf4
Emanuel Pina <20098990+emanuelpina@users.noreply.github.com>
2019-10-06 17:07:48 +0100

    Adds function to hide/show scroll up btn on resize

--- themes/mytheme/assets/js/custom.js
+++ themes/mytheme/assets/js/custom.js
@@ -1,5 +1,5 @@
 // Embed svg sprite
-$.get("/images/sprite.svg", function(data) {
+$.get('/images/sprite.svg', function(data) {
     var sprite = new XMLSerializer().serializeToString(data.documentElement);
     $('body').append(sprite);
   });
@@ -8,9 +8,9 @@ $.get("/images/sprite.svg", function(data) {
 $(window).scroll(function() {    
     var scroll = $(window).scrollTop();
     if (scroll > 0) {
-        $(".navbar-wrap").addClass("scroll");
+        $('.navbar-wrap').addClass('scroll');
     } else {
-        $(".navbar-wrap").removeClass("scroll");
+        $('.navbar-wrap').removeClass('scroll');
     }
 });
 
@@ -36,7 +36,7 @@ $(document).on('click', '.has-children > a', function(e) {
 
 // Lazyload and responsive Youtube videos
 $('.youtube').each(function(){
-	var youtubeembed = $(this).attr("data-embed");
+	var youtubeembed = $(this).attr('data-embed');
     $('[data-embed=' + youtubeembed + '] > .play').prepend('<svg class="icon-play" viewBox="0 0 68 48"><path d="M66.52,7.74c-0.78-2.93-2.49-5.41-5.42-6.19C55.79,.13,34,0,34,0S12.21,.13,6.9,1.55 C3.97,2.33,2.27,4.81,1.48,7.74C0.06,13.05,0,24,0,24s0.06,10.95,1.48,16.26c0.78,2.93,2.49,5.41,5.42,6.19 C12.21,47.87,34,48,34,48s21.79-0.13,27.1-1.55c2.93-0.78,4.64-3.26,5.42-6.19C67.94,34.95,68,24,68,24S67.94,13.05,66.52,7.74z"></path><path d="M 45,24 27,14 27,34" fill="#fff"></path></svg>');
     $('[data-embed=' + youtubeembed + ']').prepend('<img src="https://img.youtube.com/vi/' + youtubeembed + '/sddefault.jpg" />');
   $(document).on('click', '[data-embed=' + youtubeembed + ']', function() {
@@ -47,7 +47,7 @@ $('.youtube').each(function(){
 // Lazyload and responsive Twitch videos
 $('.twitch').each(function(){
 	var twitchid = '0fi1un4gx6l39gwtz3j10eeqalv2mi'; //Twich API Client ID
-	var twitchembed = $(this).attr("data-embed");
+	var twitchembed = $(this).attr('data-embed');
     $('[data-embed=' + twitchembed + '] > .play').prepend('<svg class="icon-play" viewBox="0 0 30 30" xmlns="http://www.w3.org/2000/svg"><path d="M22.447 14.105l-14-7A1 1 0 0 0 7 8v14a.999.999 0 0 0 1.447.894l14-7a1 1 0 0 0 0-1.789"></path></svg>');
 	$.getJSON('https://api.twitch.tv/kraken/videos/' + twitchembed + '?client_id=' + twitchid, function(data) {
   	var twitchthumb = data.preview;
@@ -105,7 +105,7 @@ $(window).scroll(function () {
             opacity = (windowScrollTop / (bodyHeight - windowHeight)) * 4,
             stroke = 100 - ((windowScrollTop / (bodyHeight - windowHeight)) * 100);
         if (bodyHeight > trigger) {
-            $('.scroll-up').css("opacity", opacity)
+            $('.scroll-up').css('opacity', opacity);
             $('.scroll-up-progress > circle').attr({ 'stroke-dashoffset': stroke });
         }
 });
@@ -114,4 +114,18 @@ $(document).on('click', '.scroll-up', function(e) {
         scrollspeed = (scroll * 0.133); // scroll 7500px per second
     e.preventDefault();
     $("html, body").animate({scrollTop: 0}, scrollspeed);
- });
+});
+$(window).on('resize', function(){
+    var windowScrollTop = $(window).scrollTop(),
+        windowHeight = $(window).outerHeight(),
+        bodyHeight = $(document).height(),
+        trigger = windowHeight * 2, // the page content is at least twice the size of the window
+        opacity = (windowScrollTop / (bodyHeight - windowHeight)) * 4,
+        stroke = 100 - ((windowScrollTop / (bodyHeight - windowHeight)) * 100);
+    if ( !(bodyHeight > trigger)) {
+        $('.scroll-up').css('opacity', 0);
+    } else {
+        $('.scroll-up').css('opacity', opacity);
+        $('.scroll-up-progress > circle').attr({ 'stroke-dashoffset': stroke });
+    }
+});
