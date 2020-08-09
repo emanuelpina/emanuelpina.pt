908a77880a479532495eb8f50a416d059d415143
emanuelpina <contacto@emanuelpina.pt>
2020-08-09 11:54:18 +0100

    Redesign scroll to top button

--- themes/mytheme/assets/js/custom.js
+++ themes/mytheme/assets/js/custom.js
@@ -102,36 +102,33 @@ $('.post-content > h3').each(function(){
     }
 });
 
-// Scroll up button with progress indication
+// Scroll up button
+$('.scroll-up').css('opacity', 0);
 $(window).scroll(function () {
         var windowScrollTop = $(window).scrollTop(),
             windowHeight = $(window).outerHeight(),
             bodyHeight = $(document).height(),
             trigger = windowHeight * 2, // the page content is at least twice the size of the window
-            opacity = (windowScrollTop / (bodyHeight - windowHeight)) * 4,
-            stroke = 100 - ((windowScrollTop / (bodyHeight - windowHeight)) * 100);
+            opacity = (windowScrollTop / (bodyHeight - windowHeight)) * 4
         if (bodyHeight > trigger) {
             $('.scroll-up').css('opacity', opacity);
-            $('.scroll-up-progress > circle').attr({ 'stroke-dashoffset': stroke });
         }
-});
-$(document).on('click', '.scroll-up', function(e) {
-    var scroll = $(window).scrollTop(),
-        scrollspeed = (scroll * 0.133); // scroll 7500px per second
-    e.preventDefault();
-    $("html, body").animate({scrollTop: 0}, scrollspeed);
 });
 $(window).on('resize', function(){
     var windowScrollTop = $(window).scrollTop(),
         windowHeight = $(window).outerHeight(),
         bodyHeight = $(document).height(),
         trigger = windowHeight * 2, // the page content is at least twice the size of the window
-        opacity = (windowScrollTop / (bodyHeight - windowHeight)) * 4,
-        stroke = 100 - ((windowScrollTop / (bodyHeight - windowHeight)) * 100);
+        opacity = (windowScrollTop / (bodyHeight - windowHeight)) * 4
     if ( !(bodyHeight > trigger)) {
         $('.scroll-up').css('opacity', 0);
     } else {
         $('.scroll-up').css('opacity', opacity);
-        $('.scroll-up-progress > circle').attr({ 'stroke-dashoffset': stroke });
     }
+});
+$(document).on('click', '.scroll-up a', function(e) {
+    var scroll = $(window).scrollTop(),
+        scrollspeed = (scroll * 0.133); // scroll 7500px per second
+    e.preventDefault();
+    $("html, body").animate({scrollTop: 0}, scrollspeed);
 });
