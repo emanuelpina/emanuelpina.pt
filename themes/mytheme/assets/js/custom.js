3ad8700a4bda47cb1778a2843c53b50440cb8820
Emanuel Pina <contacto@emanuelpina.pt>
2021-06-28 21:38:46 +0100

    Use vanilla JS instead of jQuery

--- themes/mytheme/assets/js/custom.js
+++ themes/mytheme/assets/js/custom.js
@@ -1,38 +1,41 @@
 // Prevent default action on click on menus with childrens if touch screen
-$('.has-children > a').on('click',function(e){
-    var isTouch = ('ontouchstart' in document.documentElement);
-    if( isTouch ){
-        e.preventDefault();
-    }
-});
+var hasChildren = document.querySelectorAll('.has-children > a');
+for (i = 0; i < hasChildren.length; i++) {
+    hasChildren[i].addEventListener('click', function(event) {
+        if ('ontouchstart' in document.documentElement) {
+            event.preventDefault();
+        }
+    });
+}
 
 // Scroll up button
-$('.scroll-up').css('opacity', 0);
-$(window).scroll(function () {
-        var windowScrollTop = $(window).scrollTop(),
-            windowHeight = $(window).outerHeight(),
-            bodyHeight = $(document).height(),
-            trigger = windowHeight * 2, // the page content is at least twice the size of the window
-            opacity = (windowScrollTop / (bodyHeight - windowHeight)) * 4
-        if (bodyHeight > trigger) {
-            $('.scroll-up').css('opacity', opacity);
-        }
-});
-$(window).on('resize', function(){
-    var windowScrollTop = $(window).scrollTop(),
-        windowHeight = $(window).outerHeight(),
-        bodyHeight = $(document).height(),
-        trigger = windowHeight * 2, // the page content is at least twice the size of the window
-        opacity = (windowScrollTop / (bodyHeight - windowHeight)) * 4
-    if ( !(bodyHeight > trigger)) {
-        $('.scroll-up').css('opacity', 0);
+document.addEventListener("scroll", handleScroll);
+
+var scrollToTopBtn = document.querySelector(".scroll-up");
+scrollToTopBtn.style.opacity = 0;
+
+function handleScroll() { 
+  var windowScrollTop = window.scrollY,
+      windowHeight = window.outerHeight,
+      bodyHeight = document.body.scrollHeight,
+      trigger = windowHeight * 2,
+      opacity = (windowScrollTop / (bodyHeight - windowHeight)) * 4
+    
+    if (bodyHeight > trigger) {
+        console.log(opacity);
+        scrollToTopBtn.style.opacity = opacity;
     } else {
-        $('.scroll-up').css('opacity', opacity);
+        console.log("no scroll button")
+        scrollToTopBtn.style.display = "none";
     }
-});
-$(document).on('click', '.scroll-up a', function(e) {
-    var scroll = $(window).scrollTop(),
-        scrollspeed = (scroll * 0.133); // scroll 7500px per second
-    e.preventDefault();
-    $("html, body").animate({scrollTop: 0}, scrollspeed);
-});
+}
+
+scrollToTopBtn.addEventListener("click", scrollToTop);
+
+function scrollToTop(event) {
+  event.preventDefault();
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
